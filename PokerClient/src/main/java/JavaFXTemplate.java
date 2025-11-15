424ec45be723855a9b80c3c30c101ba006a03ebf
sola <aakindelen132@gmail.com>
2025-11-15 08:38:12 -0600

    Forgot to update template file

--- PokerClient/src/main/java/JavaFXTemplate.java
+++ PokerClient/src/main/java/JavaFXTemplate.java
@@ -29,9 +29,19 @@ public class JavaFXTemplate extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
         try{
-            Parent gameRoot = FXMLLoader.load(getClass().getResource("/ClientFXML/WelcomSceneFXML.fxml"));
-            Scene  currentScene = ((Node) event.getSource()).getScene();
-            currentScene.setRoot(gameRoot);
+            Parent root = FXMLLoader.load(getClass().getResource("/ClientFXML/clientFXML.fxml"));
+
+
+            primaryStage.setTitle("Three-Card-Poker Join");
+
+            Scene welcomeScene = new Scene(root, 700,700);
+
+
+            welcomeScene.getStylesheets().add("/clientStyles/clientStyle_1.css");
+
+            primaryStage.setScene(welcomeScene);
+
+            primaryStage.show();
 
         }catch (Exception e){
             e.printStackTrace();
