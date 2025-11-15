6df420b979b5d43970480aacde59df41d532a6d9
sola <aakindelen132@gmail.com>
2025-11-15 13:53:36 -0600

    Poker logic small update

--- PokerServer/src/main/java/ThreeCardLogic.java
+++ PokerServer/src/main/java/ThreeCardLogic.java
@@ -3,11 +3,11 @@ import java.util.Collections;
 import java.util.Stack;
 
 class ThreeCardLogic{
-
+    private Stack<Card> deck;
     class Card {
         private char suit;
         private int value;
-        private Stack<Card> deck;
+
 
         Card(char suit, int value){
 
@@ -22,16 +22,18 @@ class ThreeCardLogic{
         }
     }
 
+    //Will implement when I understand how poker works
     public void evalHand(ArrayList<Card> hand, int wager){
 
     }
 
+    //Compare player hand with dealer and return winning amount?
    public int comparehands(ArrayList<Card> dealer, ArrayList<Card> player){
         return  0;
    }
 
    public void shuffle(){
-
+       Collections.shuffle(deck);
    }
 
    public void reset(){
